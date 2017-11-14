dcd84af417983a7370b1aa45b5a0208b51b53157
ethan <zz511627807@163.com>
2017-11-14 15:28:14 +0800

    add

---
+++ haha/haha.h
@@ -0,0 +1,14 @@
+//
+//  haha.h
+//  haha
+//
+//  Created by macOfEthan on 17/11/14.
+//  Copyright © 2017年 macOfEthan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface haha : NSObject
+
+- (void)say;
+@end
---
+++ haha/haha.m
@@ -0,0 +1,16 @@
+//
+//  haha.m
+//  haha
+//
+//  Created by macOfEthan on 17/11/14.
+//  Copyright © 2017年 macOfEthan. All rights reserved.
+//
+
+#import "haha.h"
+
+@implementation haha
+- (void)say
+{
+    NSLog(@"say");
+}
+@end
